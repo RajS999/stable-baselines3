ac54f5c30c6671b5ea6eb4fc7a5a74b83479b260
RajS999 <52235655+RajS999@users.noreply.github.com>
2021-04-07 17:52:44 +0530

    PPG in stable-baselines3 from https://github.com/janEbert/sb3-ppg/ with some changes
    PPG in stable-baselines3 from https://github.com/janEbert/sb3-ppg/ with some changes

--- stable_baselines3/__init__.py
+++ stable_baselines3/__init__.py
@@ -7,6 +7,7 @@ from stable_baselines3.her import HER
 from stable_baselines3.ppo import PPO
 from stable_baselines3.sac import SAC
 from stable_baselines3.td3 import TD3
+from stable_baselines3.ppg import PPG
 
 # Read version from file
 version_file = os.path.join(os.path.dirname(__file__), "version.txt")
---
+++ stable_baselines3/ppg/__init__.py
@@ -0,0 +1,2 @@
+from .policies import AuxMlpPolicy
+from .ppg import PPG
